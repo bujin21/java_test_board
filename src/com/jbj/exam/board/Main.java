ca3a0dc6a4215af453fef2565386a7ffb11c05d8
bujin21 <cvj0810@naver.com>
2022-10-03 22:20:32 +0900

    게시물 등록시, 마지막 게시물 번호 이후로 id 정하기

--- src/com/jbj/exam/board/Main.java
+++ src/com/jbj/exam/board/Main.java
@@ -22,6 +22,10 @@ public class Main {
 		 ArrayList<Article> articles = new ArrayList<Article>();
 
 		 makeTestData(articles);
+		 
+		 if ( articles.size() > 0) {
+		      articleLastId = articles.get(articles.size() - 1).id;
+		    }
 
 		while (true) {
 			System.out.printf("명령) ");

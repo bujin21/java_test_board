1b137ebe54921dd41d326309dbab89d542b3a291
bujin21 <cvj0810@naver.com>
2022-10-03 22:56:43 +0900

    새로 생선된 게시물을 articles에 추가하고 lastArticle 변수의 필요성을 제거

--- src/com/jbj/exam/board/Main.java
+++ src/com/jbj/exam/board/Main.java
@@ -49,12 +49,12 @@ public class Main {
 
 		      }else if (cmd.equals("/usr/article/detail")) {
 
-				if (lastArticle == null) {
+				if (articles.isEmpty()) {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
 
-				Article article = lastArticle;
+				Article article = articles.get(articles.size() -1);
 
 				System.out.println("- 게시물 상세 내용 -");
 				System.out.printf("번호 : %d\n", article.id);
@@ -72,7 +72,10 @@ public class Main {
 				int id = ++articleLastId;
 
 				Article article = new Article(id, title, body);
+				
 				lastArticle = article;
+				
+				articles.add(article);
 				System.out.println("생성된 게시물 객체 : " + article);
 				System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
 			} else {

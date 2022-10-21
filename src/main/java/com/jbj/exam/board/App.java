91bfecb0f2da217db947260809758c776f3e552f
bujin21 <cvj0810@naver.com>
2022-10-21 14:28:58 +0900

    테스트를 위한 자동 로그인 기능 구현

--- src/main/java/com/jbj/exam/board/App.java
+++ src/main/java/com/jbj/exam/board/App.java
@@ -9,76 +9,77 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-	  void run() {
+  private void forTestLoginByMemberId(int id) {
+    Member member = Container.getMemberService().getMemberById(id);
+    new Rq().login(member);
+  }
 
-	    Scanner sc = Container.getSc();
+  public void run() {
 
-	    System.out.println("== 게시판 v 0.1 ==");
-	    System.out.println("== 프로그램 시작 ==");
+    Scanner sc = Container.getSc();
 
-	    
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
 
-	    while (true) {
-	    	Rq rq = new Rq();
+    forTestLoginByMemberId(1);
 
-	    	String promptName = "명령";
-	    	
-	    	if( rq.isLogined()) {
-					Member loginedMember = rq.getLoginedMember();
-	    		promptName = loginedMember.getLoginId();
-	    	}
-	    	
-	    	System.out.printf("%s) ", promptName);
-	    	String cmd = sc.nextLine();
+    while (true) {
+      Rq rq = new Rq();
 
-				rq.setCommand(cmd);
+      String promptName = "명령";
+      if (rq.isLogined()) {
+        Member loginedMember = rq.getLoginedMember();
+        promptName = loginedMember.getLoginId();
+      }
 
-				if(runInterceptors(rq) == false){
-					continue;
-				}
+      System.out.printf("%s) ", promptName);
+      String cmd = sc.nextLine();
 
-	      if (rq.getUrlPath().equals("exit")) {
-	        break;
-	      } else if (rq.getUrlPath().equals("/usr/article/list")) {
-	    	 Container.getUsrArticleController().actionList(rq);
-	      } else if (rq.getUrlPath().equals("/usr/article/detail")) {
-	    	  Container.getUsrArticleController().actionDetail(rq);
-	      } else if (rq.getUrlPath().equals("/usr/article/write")) {
-	    	  Container.getUsrArticleController().actionWrite(rq);
-	      } else if (rq.getUrlPath().equals("/usr/article/modify")) {
-	    	  Container.getUsrArticleController().actionModify(rq);
-	      } else if (rq.getUrlPath().equals("/usr/article/delete")) {
-	    	  Container.getUsrArticleController().actionDelete(rq);
-	      } else if (rq.getUrlPath().equals("/usr/member/join")) {
-	          Container.getUsrMemberController().actionJoin(rq);
-	      } else if (rq.getUrlPath().equals("/usr/member/login")) {
-	          Container.getUsrMemberController().actionLogin(rq);
-	      } else if (rq.getUrlPath().equals("/usr/member/logout")) {
-	          Container.getUsrMemberController().actionLogout(rq);
-	      } else {
-	        System.out.printf("입력 된 명령어 : %s\n", cmd);
-	      }
-	    }
+      rq.setCommand(cmd);
 
-	    System.out.println("== 프로그램 끝 ==");
+      if (runInterceptors(rq) == false) {
+        continue;
+      }
 
-	    sc.close();
-	  }
+      if (rq.getUrlPath().equals("exit")) {
+        break;
+      } else if (rq.getUrlPath().equals("/usr/article/list")) {
+        Container.getUsrArticleController().actionList(rq);
+      } else if (rq.getUrlPath().equals("/usr/article/detail")) {
+        Container.getUsrArticleController().actionDetail(rq);
+      } else if (rq.getUrlPath().equals("/usr/article/write")) {
+        Container.getUsrArticleController().actionWrite(rq);
+      } else if (rq.getUrlPath().equals("/usr/article/modify")) {
+        Container.getUsrArticleController().actionModify(rq);
+      } else if (rq.getUrlPath().equals("/usr/article/delete")) {
+        Container.getUsrArticleController().actionDelete(rq);
+      } else if (rq.getUrlPath().equals("/usr/member/join")) {
+        Container.getUsrMemberController().actionJoin(rq);
+      } else if (rq.getUrlPath().equals("/usr/member/login")) {
+        Container.getUsrMemberController().actionLogin(rq);
+      } else if (rq.getUrlPath().equals("/usr/member/logout")) {
+        Container.getUsrMemberController().actionLogout(rq);
+      } else {
+        System.out.printf("입력 된 명령어 : %s\n", cmd);
+      }
+    }
 
-	private boolean runInterceptors(Rq rq) {
-		List<Interceptor> interceptors = new ArrayList<>();
+    System.out.println("== 프로그램 끝 ==");
+    sc.close();
+  }
 
-		interceptors.add(Container.getNeedLoginInterceptor());
-		interceptors.add(Container.getNeedLogoutInterceptor());
+  private boolean runInterceptors(Rq rq) {
+    List<Interceptor> interceptors = new ArrayList<>();
 
-		for(Interceptor interceptor : interceptors){
-			if(interceptor.run(rq) == false){
-				return false;
-			}
-		}
+    interceptors.add(Container.getNeedLoginInterceptor());
+    interceptors.add(Container.getNeedLogoutInterceptor());
 
-		return true;
-	}
+    for (Interceptor interceptor : interceptors) {
+      if (interceptor.run(rq) == false) {
+        return false;
+      }
+    }
 
-
+    return true;
+  }
 }
--- src/main/java/com/jbj/exam/board/service/MemberService.java
+++ src/main/java/com/jbj/exam/board/service/MemberService.java
@@ -26,4 +26,8 @@ public class MemberService {
   public Member getMemberByLoginId(String loginId) {
     return memberRepository.getMemberByLoginId(loginId);
   }
+
+  public Member getMemberById(int id) {
+    return memberRepository.getMemberById(id);
+  }
 }
